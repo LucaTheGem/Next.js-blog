998237a5ae6bb8926de8bf38825d31ca3ddbb357
Ben Lim <lucakingoff@gmail.com>
2023-10-05 13:44:14 -0400

    create GetStaticsProps component to get Posts at build time

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e542f1fd54b17a1a"; }
+/******/ 		__webpack_require__.h = function() { return "4bafc7388d95c8cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
